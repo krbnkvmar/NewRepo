c4c394e19a044fe10eba62e5cb68a3fa1afb2585
krbnkvmar <sherwood.off@gmail.com>
2025-09-17 20:12:20 +0500

    что-то

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("хай хай, мир");
+            Console.ResetColor();
+        }
+    }
+}
